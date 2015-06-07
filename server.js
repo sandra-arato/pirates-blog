29244b3a2f94448d6aa0bd0c1cc781fb7a5365c2
Sandra Szenti <alexandraszenti@gmail.com>
2015-06-07 21:33:43 +0100

    keeping alive the server

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-var path = require('path');
-var ghost = require('ghost');
+var path = require('path'),
+	ghost = require('ghost'),
+	http = require('http');
 
 ghost({
   config: path.join(__dirname, 'config.js')
 }).then(function (ghostServer) {
   ghostServer.start();
 });
+
+
+setInterval(function() {
+    http.get('http://ghost-pirates.herokuapp.com');
+}, 300000);
